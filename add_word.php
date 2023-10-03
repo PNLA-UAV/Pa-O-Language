bc5b7bdc2baf9f0a3dbf9b171447f74a61e7b094
ခွန်ဖန်ဒွဲ့ <127810188+KhunPhanDuae@users.noreply.github.com>
2023-10-04 02:17:52 +0630

    Create add_word.php

---
+++ add_word.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $word = $_POST["word"];
+    $definition = $_POST["definition"];
+    
+    // Insert the new word and definition into your database
+    // Implement the database connection and insertion logic here.
+}
+?>
