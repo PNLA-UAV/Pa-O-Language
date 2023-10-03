6c21938ad84a5ff918bb7c4b63fa1b684fa70a79
ခွန်ဖန်ဒွဲ့ <127810188+KhunPhanDuae@users.noreply.github.com>
2023-10-04 01:59:28 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const searchForm = document.getElementById("search-form");
     const searchInput = document.getElementById("search-input");
-    const searchButton = document.getElementById("search-button");
     const wordDefinition = document.getElementById("word-definition");
 
     searchForm.addEventListener("submit", function (e) {
         e.preventDefault();
         const searchTerm = searchInput.value;
-        // Perform a JavaScript AJAX request to retrieve the word definition and display it in 'wordDefinition' div
-        // You'll need to implement this part.
+
+        // Perform an AJAX request to get the definition
+        fetch("get_definition.php", {
+            method: "POST",
+            body: new URLSearchParams({ word: searchTerm }),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        })
+        .then((response) => response.text())
+        .then((data) => {
+            wordDefinition.textContent = data; // Display the definition
+        })
+        .catch((error) => {
+            console.error("Error fetching definition:", error);
+        });
     });
 });
-                          
+              
